86e54edca749ff5f9b8b5968c28695db49ade85e
Daniele Lecce <daniele.lecce.hoverture@skytv.it>
2025-11-13 13:15:23 +0100

    Refactor App components to include StylesProvider with generateClassName for consistent styling

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
 export default function App() {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: 'container',
+  });
+
   return (
-    <BrowserRouter>
-      <div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
         <Header />
         <MarketingApp />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </StylesProvider>
   );
 } 
