4ac28a2b745a789986e6ef27017a81b86fc47d26
Daniele Lecce <daniele.lecce.hoverture@skytv.it>
2025-11-13 17:09:43 +0100

    Refactor MarketingApp and bootstrap to use history for navigation and pass history prop to App component

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,10 +1,27 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function MarketingApp() {
   const ref = useRef(null);
+  const history = useHistory();
+
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+
+    const { pathname: initialPathname } = history.location;
+    if (initialPathname !== '/') {
+      history.push(initialPathname);
+    }
   }, []);
 
   return <div ref={ref} />;
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-export default function App() {
+export default function App({ history }) {
   const generateClassName = createGenerateClassName({
     productionPrefix: 'marketing',
   });
 
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route path="/pricing" component={Pricing} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 }
