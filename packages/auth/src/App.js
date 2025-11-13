ef403c56e6ac82b0da90addc5edb3e00176a6d27
Daniele Lecce <daniele.lecce.hoverture@skytv.it>
2025-11-13 18:03:18 +0100

    Implement authentication module with SignIn and SignUp components, configure routing, and set up Webpack for module federation

---
+++ packages/auth/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+export default function App({ history }) {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: 'auth',
+  });
+
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
+        <Switch>
+          <Route path="/auth/signin" component={Signin} />
+          <Route path="/auth/signup" component={Signup} />
+        </Switch>
+      </Router>
+    </StylesProvider>
+  );
+}
