f6ef9155566310bac537943899ca86eef6764539
Daniele Lecce <daniele.lecce.hoverture@skytv.it>
2025-11-14 22:37:51 +0100

    feat(container): add news remote entry to Module Federation configuration

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -19,6 +19,7 @@ const prodConfig = {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
         auth: `auth@${domain}/auth/latest/remoteEntry.js`,
         dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`,
+        news: `news@${domain}/news/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
