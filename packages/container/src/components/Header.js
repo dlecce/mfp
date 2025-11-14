83c0ec7ab40163b68f1a66a809e67332e9b064b7
Daniele Lecce <daniele.lecce.hoverture@skytv.it>
2025-11-14 11:07:53 +0100

    Fix Header component appBar background color for consistency

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -19,6 +19,7 @@ const useStyles = makeStyles((theme) => ({
   },
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
+    backgroundColor: 'white !important' // lock in desired color
   },
   toolbar: {
     flexWrap: 'wrap',
